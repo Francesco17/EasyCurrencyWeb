949d706a51124338980273ec055e771e3732a2eb
Francesco Fuggitti <francesco@Francescos-MBP.homenet.telecomitalia.it>
2017-11-26 11:46:53 +0100

    currency converter done, but limited api only for usd.

--- js/exchange.js
+++ js/exchange.js
@@ -1,8 +1,17 @@
+function currencyconverter(){
+
+var val1 = document.getElementById('val1').value;
 // set endpoint and your access key
 endpoint = 'live'
 access_key = '36ed860ff64b5e9961d86f10bc053a6f';
+source = "USD";
+
 e = document.getElementById("curr_1");
-source = e.options[e.selectedIndex].value;
+from = e.options[e.selectedIndex].value;
+
+e1 = document.getElementById("curr_2");
+to = e1.options[e1.selectedIndex].value;
+combine = from+to;
 
 // get the most recent exchange rates via the "live" endpoint:
 $.ajax({
@@ -11,13 +20,17 @@ $.ajax({
     success: function(json) {
 
         // exchange rata data is stored in json.quotes
-        alert(json.quotes.USDGBP);
+        var rate = json.quotes[combine];
+        document.getElementById('val2').value = val1*rate;
+
+
 
         // source currency is stored in json.source
-        alert(json.source);
+        //alert(json.source);
 
         // timestamp can be accessed in json.timestamp
-        alert(json.timestamp);
+        //alert(json.timestamp);
 
     }
 });
+}
