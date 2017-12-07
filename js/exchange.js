ba1a7ea508a8e1a8ca23c9b8e64c2f0be091ed94
Francesco Fuggitti <francesco@Francescos-MBP.homenet.telecomitalia.it>
2017-12-07 22:39:16 +0100

    library exchange modified. currency converter call in index.html modified.

--- js/exchange.js
+++ js/exchange.js
@@ -1,6 +1,6 @@
 // funzione chiamata al click del bottone "converti"
 
-function currencyconverter(){
+function currencyconverter(from, to, val1, result){
 
 // definisco risorsa, access key e sorgente
 endpoint = 'live'
@@ -8,16 +8,17 @@ access_key = '36ed860ff64b5e9961d86f10bc053a6f';
 source = "USD";
 
 // valore da convertire
-var val1 = document.getElementById('val1').value;
+// var val1 = document.getElementById('val1').value;
+var conversione;
 
 if (val1 == '') {
   return false;
 }
 
 // prendo la valuto di partenza
-from = document.getElementById("curr_1").options[document.getElementById("curr_1").selectedIndex].value;
+// from = document.getElementById("curr_1").options[document.getElementById("curr_1").selectedIndex].value;
 // prendo la valuta di arrivo
-to = document.getElementById("curr_2").options[document.getElementById("curr_2").selectedIndex].value;
+// to = document.getElementById("curr_2").options[document.getElementById("curr_2").selectedIndex].value;
 
 // eseguo la chiamata ajax alle api
 // il piano gratuito delle api permette di selezionare come sorgente solo USD
@@ -30,23 +31,27 @@ $.ajax({
         // se la valuta di partenza è uguale ad USD allora moltiplico il valore per il cambio
         if (from == source) {
           combine = from+to;
-          document.getElementById('val2').value = val1*json.quotes[combine];
+          // document.getElementById('val2').value = val1*json.quotes[combine];
+          conversione = val1*json.quotes[combine];
         }
         // se la valuta di arrivo è uguale ad USD allora divido il valore per il cambio
         else if (to == source) {
           combine = to+from;
-          document.getElementById('val2').value = val1/json.quotes[combine];
+          // document.getElementById('val2').value = val1/json.quotes[combine];
+          conversione = val1/json.quotes[combine];
         }
         else {
           // altrimenti effettue 2 cambi: partenza ad USD e da USD ad arrivo
           combine = source+from;
           var temp = val1/json.quotes[combine];
           combine = source+to;
-          document.getElementById('val2').value = temp*json.quotes[combine];
+          // document.getElementById('val2').value = temp*json.quotes[combine];
+          conversione = temp*json.quotes[combine];
         }
-
+        result(conversione);
     }
-});
+
+  });
 }
 //unzione che inverte le valute e ricalcola la conversione
 function invert_curr(){
@@ -56,11 +61,16 @@ function invert_curr(){
   // prendo la valuta di arrivo
   to = document.getElementById("curr_2").options[document.getElementById("curr_2").selectedIndex].value;
 
+  //prendo il valore
+  value = document.getElementById("val1").value;
+
   //scambio le valute
   $('#curr_2').val(from);
   $('#curr_1').val(to);
 
   //ricalcolo al conversione
-  currencyconverter();
+  currencyconverter(from, to, value, function(output){
+    document.getElementById('val2').value = output;
+  });
 
 }
