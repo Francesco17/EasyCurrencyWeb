ff177a2cdeec442da05dd3fa45b698c649570b3b
Francesco Fuggitti <francesco@Francescos-MacBook-Pro.local>
2017-12-17 19:34:28 +0100

    modified library and adjusted features of realtime_curr.html

--- js/cambistorici.js
+++ js/cambistorici.js
@@ -1,6 +1,6 @@
 // funzione chiamata al click del bottone "converti"
 
-function currencyconverter(){
+function currencyconverter( from, to, val1, date, result){
 
 // definisco risorsa, access key e sorgente
 // esempi
@@ -11,53 +11,36 @@ function currencyconverter(){
 endpoint = 'historical';
 access_key = 'cdaa48c0799e268d91b98681529912eb';
 source = "USD";
+currencies = from+","+to;
 
 
 // data storica del cambio date = '2017-01-01';
 
-date = document.getElementById("giorno").value;
+// date = document.getElementById("giorno").value;
 
 // valore da convertire
-var val1 = document.getElementById('val1').value;
-var conversione
+// var val1 = document.getElementById('val1').value;
+var conversione;
 if (val1 == '') { return false; }
 
 // prendo la valuto di partenza
-from = document.getElementById("curr_1").options[document.getElementById("curr_1").selectedIndex].value;
+// from = document.getElementById("curr_1").options[document.getElementById("curr_1").selectedIndex].value;
 // prendo la valuta di arrivo
-to = document.getElementById("curr_2").options[document.getElementById("curr_2").selectedIndex].value;
+// to = document.getElementById("curr_2").options[document.getElementById("curr_2").selectedIndex].value;
 
 // eseguo la chiamata ajax alle api
 // il piano gratuito delle api permette di selezionare come sorgente solo USD
 $.ajax({
-    url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key + '&date=' + date + '&source='+ source,
+    url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key + '&date=' + date + '&source='+ source + '&currencies='+ currencies,
     dataType: 'jsonp',
     success: function(json) {
         // exchange rata data is stored in json.quotes
         // se la valuta di partenza è uguale ad USD allora moltiplico il valore per il cambio
-        if (from == source) {
-          combine = from+to;
-	         document.getElementById('val2').value = val1*json.quotes[combine];
-          conversione = val1*json.quotes[combine];
-        }
-        // se la valuta di arrivo è uguale ad USD allora divido il valore per il cambio
-        else if (to == source) {
-          combine = to+from;
-	         document.getElementById('val2').value = val1/json.quotes[combine];
-          conversione = val1/json.quotes[combine];
-        }
-        else {
-          // altrimenti effettue 2 cambi: partenza ad USD e da USD ad arrivo
-          combine = source+from;
-          var temp = val1/json.quotes[combine];
-          combine = source+to;
-	         document.getElementById('val2').value = temp*json.quotes[combine];
-          conversione = temp*json.quotes[combine];
-        }
-//   document.getElementById('val2').value = temp*json.quotes[combine];
+        conversione = conversion(from, to, val1, json);
+        //   document.getElementById('val2').value = temp*json.quotes[combine];
 
-	console.log(conversione);
-	return conversione;
+	// console.log(conversione);
+	result(conversione);
     }
 });
 }
