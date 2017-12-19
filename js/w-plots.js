a024f7ad0084c594693c6ae497f4bfdb2a188957
Francesco Fuggitti <francesco@Francescos-MBP.homenet.telecomitalia.it>
2017-12-19 11:31:59 +0100

    added logic for plots. logic put inside an external js file

---
+++ js/w-plots.js
@@ -0,0 +1,55 @@
+function trans_plot(_user, flag){
+
+  var domain = [];
+  var codomain = [];
+  var data;
+  var bar_width = [];
+  var base_currency = _user.base_currency;
+
+  for (var i = 0; i < _user.transaction.length; i++) {
+    domain[i] = _user.transaction[i].valuta;
+    codomain[i] = _user.transaction[i].valore;
+    bar_width[i] = 0.5;
+  }
+
+  if (flag) {
+
+    data = [{
+            x: domain,
+            y: codomain,
+            type: 'bar',
+            width: bar_width
+          }];
+
+          var layout = {
+          title: 'Transazioni effettuate',
+          xaxis: {},
+          yaxis: {
+        title: base_currency,
+        }
+      };
+
+      $('#plot-div').fadeIn();
+      Plotly.newPlot('plot-div', data, layout);
+  }
+  else {
+    var data = [{
+      values: codomain,
+      labels: domain,
+      type: 'pie'
+    }];
+
+    var layout = {
+      title: 'Transazioni effettuate'
+    };
+
+    $('#plot-div').fadeIn();
+    Plotly.newPlot('plot-div', data, layout);
+  }
+
+}
+
+
+function balance_plot(){
+
+}
